516eafdf542143d79d8da65f25f737d97a91524c
sulivanShu <regivanx@gmail.com>
2026-01-24 13:40:59 +0100

    Ajout d'une option pour permettre à l'utilisateur de choisir sa seed

--- src/seed.jl
+++ src/seed.jl
@@ -1,3 +1,12 @@
+using ArgParse
+settings = ArgParseSettings()
+@add_arg_table settings begin
+    "--seed"
+        help = "choose a random integer"
+        arg_type = Int
+        default = 0
+end
+parsed_args = parse_args(settings)
+my_seed = parsed_args["seed"]
 Random.seed!(my_seed)
 @info "Your seed is: $my_seed."
-@info "Open scripts/main.jl and change your seed if you want."
